d7a76a2cd4947932299fab7586062698b514c50e
Harish <harish@server.com>
2026-01-30 13:14:30 +0530

    rector install + results

---
+++ rector.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withTypeCoverageLevel(50)
+    ->withDeadCodeLevel(50)
+    ->withCodeQualityLevel(50)
+    ->withPhpVersion(80212);
+
